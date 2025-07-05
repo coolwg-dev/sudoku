dba0290889edc7edc7cad3f781cf1f98555d01ff
coolwong-dev <coolwong@connect.hku.hk>
2025-07-05 18:49:25 +0800

    add notification of wrong or finish game

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.sudoku;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SudokuBoardView.SudokuListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SudokuBoardView sudokuBoard = findViewById(R.id.sudokuBoard);
+        sudokuBoard.setSudokuListener(this);
         int[] buttonIds = {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9};
         for (int i = 0; i < buttonIds.length; i++) {
             final int number = i + 1;
@@ -22,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public void onNumberWrong() {
+        Toast.makeText(this, "Wrong number!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSudokuCompleted() {
+        Toast.makeText(this, "Congratulations! Sudoku completed!", Toast.LENGTH_LONG).show();
     }
 }
